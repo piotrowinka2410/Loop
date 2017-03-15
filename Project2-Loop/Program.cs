bb54277663f73f408b800541f3198e298fdebd39
piotrowinka2410 <piotrowinka2410@gmail.com>
2017-03-15 17:43:13 +0100

    Loop

---
+++ Project2-Loop/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                // Pętla for - uzytkownik podaje liczbe i wyswietlane sa gwiazdki
+                //Console.WriteLine("Podaj liczbe");
+                //double s = double.Parse(Console.ReadLine());
+                //for (double i = 0; i < s; i++)
+                //{
+                //    Console.WriteLine("*");
+                //}
+
+
+
+
+                /* pętla for - użytkownik pobiera 2 liczby i powstaje kwadrat lub prostokąt 
+                 Console.WriteLine("Podaj 2 liczby");
+                 double a = double.Parse(Console.ReadLine());
+                 double b = double.Parse(Console.ReadLine());
+                for (double i = 0; i < a; i++)
+                 {
+                 for (double j = 0; j < b; j++)
+                 {
+                     Console.Write("*");
+                 }
+              Console.WriteLine();
+                 }
+                 } */
+
+
+
+
+                /* Pętla while - uzytkownik podaje liczbe i wyswietlane sa gwiazdki
+                  Console.WriteLine("Podaj liczbe");
+                double s = double.Parse(Console.ReadLine());
+                double i=0;
+                while (i<s)
+                {
+                    i++;
+                    Console.WriteLine("*");
+                }       */
+
+
+
+
+                // pętla while - użytkownik pobiera 2 liczby i powstaje kwadrat lub prostokąt 
+                //Console.WriteLine("Podaj 2 liczby");
+                //double a = double.Parse(Console.ReadLine());
+                //double b = double.Parse(Console.ReadLine());
+                //double i = 0;
+                //double j = 0;
+
+                //while (i < a)
+                //{
+                //    i++;
+                //    {
+                //        while (j < b)
+                //        {
+                //            Console.Write("*");
+                //            j++;
+                //        }
+                //        j = 0;
+                //    }
+                //    Console.WriteLine("*");
+                //}
+
+
+
+                // Pętla do while - uzytkownik podaje liczbe i wyswietlane sa gwiazdki
+                //    Console.WriteLine("Podaj liczbe");
+                //    double s = double.Parse(Console.ReadLine());
+                //    double i = 0;
+                //    do
+                //    {
+                //        Console.WriteLine("*");
+                //        i++;
+                //    } while (i < s);
+                //}
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
+
